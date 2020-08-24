08daa3d72db33de2686a659c8822fced0483a8e3
Dacha <kuzivladimir@rambler.ru>
2020-08-24 19:25:53 +0300

    '2'

--- currency.py
+++ currency.py
@@ -1,8 +1,13 @@
 import bs4
 from decimal import Decimal
+import requests
+from decimal import Decimal
 
 
-def convert(amount, cur_from, cur_to, date, requests):
+
+
+
+def convert(amount, cur_from, cur_to, date):
 
     response = requests.get(f'https://www.cbr.ru/scripts/XML_daily.asp?date_req={date}')  # Использовать переданный requests
     soup = bs4.BeautifulSoup(response.content, 'xml')
@@ -14,3 +19,7 @@ def convert(amount, cur_from, cur_to, date, requests):
     amount = amount / (Decimal(cur_to[0].replace(',','.')) / Decimal(cur_to[1]))
 
     return amount.quantize(Decimal("1000.1000")) # не забыть про округление до 4х знаков после запятой
+
+
+if __name__ == '__main__':
+    result = convert(Decimal("1.00"), 'EUR', 'JPY', "24/08/2020")
